5b3e6f11e145fbcd73644fdf47848ecca1625524
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2017-10-19 22:20:08 -0700

    initial commit

--- src/LabelledArrays.jl
+++ src/LabelledArrays.jl
@@ -1,5 +1,85 @@
 module LabelledArrays
 
-# package code goes here
+using StaticArrays
+
+abstract type LabelledVector{N,T} <: FieldVector{N,T} end
+
+# LabelledVector Macros
+
+"""
+    @LVector Type Names
+    @LVector Type Names Values
+
+Creates a `LabelledVector` with names determined from the `Names`
+vector and values determined from the `Values` vector (if no values are provided,
+it defaults to not setting the values like `similar`). All of the values are converted
+to the type of the `Type` input.
+
+For example:
+
+    a = @LVector Float64 [a,b,c]
+    b = @LVector Float64 [a,b,c] [1,2,3]
+"""
+macro LVector(T,_names,vals=nothing)
+    names = Symbol.(_names.args)
+    type_name = gensym(:LVector)
+    construction_call = vals==nothing ?
+    quote
+        ($(type_name))()
+    end : quote
+        ($(type_name))($(vals)...)
+    end
+
+    quote
+        struct $(type_name) <: LabelledVector{$(length(names)),$T}
+            $((:($n::$(T)) for n in names)...)
+            $(type_name)() = new()
+            $(type_name)($((:($n) for n in names)...)) = new($((:($n) for n in names)...))
+        end
+        $(esc(construction_call))
+    end
+end
+
+"""
+    @MLVector Type Names
+    @MLVector Type Names Values
+
+Creates a `LabelledVector` with names determined from the `Names`
+vector and values determined from the `Values` vector (if no values are provided,
+it defaults to not setting the values like `similar`). All of the values are converted
+to the type of the `Type` input.
+
+For example:
+
+    a = @MLVector Float64 [a,b,c]
+    b = @MLVector Float64 [a,b,c] [1,2,3]
+"""
+macro MLVector(T,_names,vals=nothing)
+    names = Symbol.(_names.args)
+    type_name = gensym(:MLVector)
+    construction_call = vals==nothing ?
+    quote
+        ($(type_name))()
+    end : quote
+        ($(type_name))($(vals)...)
+    end
+
+    quote
+        mutable struct $(type_name) <: LabelledVector{$(length(names)),$T}
+            $((:($n::$(T)) for n in names)...)
+            $(type_name)() = new()
+            $(type_name)($((:($n) for n in names)...)) = new($((:($n) for n in names)...))
+        end
+        $(esc(construction_call))
+    end
+end
+
+# Fix broadcast https://github.com/JuliaArrays/StaticArrays.jl/issues/314
+function StaticArrays.similar_type(::Type{V}, ::Type{T}, ::Size{N}) where
+                                                        {V<:LabelledVector,T,N}
+    V
+end
+
+export LabelledVector, @LVector, @MLVector
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,32 @@
 using LabelledArrays
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+a = @LVector Float64 [a,b,c]
+b = @LVector Float64 [a,b,c] [1,2,3]
+
+@test b.a == 1
+@test b.b == 2
+@test b.c == 3
+@test b[1] == b.a
+@test b[2] == b.b
+@test b[3] == b.c
+
+@test_throws ErrorException fill!(a,1)
+@test typeof(b) <: LabelledVector{3,Float64}
+b.+b
+
+
+c = @MLVector Float64 [a,b,c]
+d = @MLVector Float64 [a,b,c] [1,2,3]
+
+@test d.a == 1
+@test d.b == 2
+@test d.c == 3
+@test d[1] == d.a
+@test d[2] == d.b
+@test d[3] == d.c
+
+@test typeof(c) <: LabelledVector{3,Float64}
+c.+d
+c.=2d
+@test c == [2,4,6]
