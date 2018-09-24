bc4f03eca099c59342f9f295aa63c2ee15680028
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-09-24 14:56:10 -0700

    Update LabelledArrays.jl

--- src/LabelledArrays.jl
+++ src/LabelledArrays.jl
@@ -2,17 +2,6 @@ module LabelledArrays
 
 using StaticArrays
 
-#=
-Base.@pure function symbol_to_index(names, s::Symbol)
-    i = 0
-    for n in names
-        i += 1
-        n == s && return i
-    end
-    return error("Label not valid")
-end
-=#
-
 include("slvector.jl")
 include("lvector.jl")
 
