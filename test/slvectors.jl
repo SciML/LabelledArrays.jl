996adb3bc31326704d3c88deea776a8b35aeb99c
Xingjian Guo <xg703@nyu.edu>
2018-11-06 11:36:43 -0500

    Update tests

--- test/slvectors.jl
+++ test/slvectors.jl
@@ -1,7 +1,7 @@
 using LabelledArrays
 using Test
 
-@SLVector ABC Int [a,b,c]
+ABC = @SLVector Int (:a,:b,:c)
 b = ABC(1,2,3)
 
 @test b.a == 1
@@ -12,5 +12,15 @@ b = ABC(1,2,3)
 @test b[3] == b.c
 
 @test_throws UndefVarError fill!(a,1)
-@test typeof(b) <: SLVector{3,Int}
-typeof(b.+b) <: ABC
+@test typeof(b.__x) == SVector{3,Int}
+
+# Type stability tests
+ABC_fl = @SLVector Float64 (:a, :b, :c)
+b_fl = ABC_fl(1.0, 2.0, 3.0)
+@test similar_type(b, Float64) == typeof(b_fl)
+@test typeof(copy(b)) == typeof(b)
+@test typeof(Float64.(b)) == typeof(b_fl)
+@test typeof(b .+ b) == typeof(b)
+@test typeof(b .+ 1.0) == typeof(b_fl)
+@test typeof(zero(b)) == typeof(b)
+@test similar(b) isa MArray # similar should return a mutable copy
