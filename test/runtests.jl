f5219b3109ae4bf4c5062d940b79482187d6cba1
Rafael Schouten <rafaelschouten@gmail.com>
2020-10-27 03:36:02 +1100

    remove sliced arrays

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,6 +7,4 @@ using InteractiveUtils
 @time @testset "SLArrays" begin include("slarrays.jl") end
 @time @testset "LArrays" begin include("larrays.jl") end
 @time @testset "DiffEq" begin include("diffeq.jl") end
-#@time @testset "LSliced" begin include("lsliced.jl") end
-#@time @testset "SLSliced" begin include("slsliced.jl") end
 end
