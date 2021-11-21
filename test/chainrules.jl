c4a4febb9def00f42cd91502d55bb53917a84733
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2021-11-21 01:43:08 +0000

    added tests for chain rules

---
+++ test/chainrules.jl
@@ -0,0 +1,18 @@
+@begin "Vector" begin
+    x = randn(2)
+    syms = (:a, :b)
+
+    # Constructors
+    test_rrule(LArray{syms}, x)
+    constructor = @SLArray (2, ) syms
+    test_rrule(constructor, x)
+
+    # `getproperty`
+    lx = LArray{syms}(x)
+    test_rrule(getproperty, lx, first(syms))
+    test_rrule(getproperty, lx, last(syms))
+
+    slx = constructor(x)
+    test_rrule(getproperty, slx, first(syms))
+    test_rrule(getproperty, slx, last(syms))
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,9 +2,11 @@ using LabelledArrays
 using Test
 using StaticArrays
 using InteractiveUtils
+using ChainRulesTestUtils
 
 @time begin
 @time @testset "SLArrays" begin include("slarrays.jl") end
 @time @testset "LArrays" begin include("larrays.jl") end
 @time @testset "DiffEq" begin include("diffeq.jl") end
+@time @testset "ChainRules" begin include("chainrules.jl") end
 end
