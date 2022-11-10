e252d79d07c83bf59fe947b3bb55a213e91b8692
ArnoStrouwen <arno.strouwen@telenet.be>
2022-11-10 15:21:13 +0100

    reproducible docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,8 @@
 using Documenter, LabelledArrays
 
+cp("./docs/Manifest.toml", "./docs/src/assets/Manifest.toml", force = true)
+cp("./docs/Project.toml", "./docs/src/assets/Project.toml", force = true)
+
 include("pages.jl")
 
 makedocs(sitename = "LabelledArrays.jl",
