801348001abdd7e10ed30531a674fb0ef87b6fbd
Arno Strouwen <arno.strouwen@telenet.be>
2023-12-03 07:18:49 +0100

    Aqua + typos CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,7 @@ using InteractiveUtils
 using ChainRulesTestUtils
 
 @time begin
+    @time @testset "Quality Assurance" begin include("qa.jl") end
     @time @testset "SLArrays" begin include("slarrays.jl") end
     @time @testset "LArrays" begin include("larrays.jl") end
     @time @testset "DiffEq" begin include("diffeq.jl") end
