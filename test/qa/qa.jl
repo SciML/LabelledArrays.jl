63435aa3c2874e7514e1e5c625f46594ac699600
ChrisRackauckas-Claude <accounts@chrisrackauckas.com>
2026-08-26 15:36:06 -0400

    qa: ignore required non-public Base/StaticArrays API in ExplicitImports

--- test/qa/qa.jl
+++ test/qa/qa.jl
@@ -1,3 +1,15 @@
 using SciMLTesting, LabelledArrays
 
-run_qa(LabelledArrays)
+# These qualified accesses are non-public API of Base/StaticArrays/ForwardDiff that
+# LabelledArrays must use to implement the broadcast, indexing, and linear algebra
+# interfaces (e.g. Base.BroadcastStyle/Base.dataids overloads, StaticArrays.LU dispatch).
+run_qa(LabelledArrays;
+    ei_kwargs = (;
+        all_qualified_accesses_are_public = (;
+            ignore = (
+                :_propagate_inbounds_meta, :AbstractArrayStyle, :BroadcastStyle,
+                :Dual, :LU, :dataids, :print_array, :size_tuple,
+            ),
+        ),
+    ),
+)
